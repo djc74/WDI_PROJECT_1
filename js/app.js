e090d7f61445b06a9c83dd4e707ed2f4278ddd16
djc74 <davidcooper974@gmail.com>
2017-10-09 11:28:11 +0100

    score updating

--- js/app.js
+++ js/app.js
@@ -2,54 +2,58 @@
 // add class to make colour
 // add click event
 // remove class and click event after set time
-// repeat
 // update score
+// repeat random selection
+
 // stop game when score is 10
 
 // console.log("hello");
 
 
 $(() => {
-
+  let score = 0;
   // Get DOM variables
   const $li = $('li');
+  const $score = $('.score');
 
   let $randomSquare;
   let timer;
 
-  // Pick random square
+  // Pick random square and add new class
   function pickRandom () {
-    $randomSquare = $($li[Math.floor($li.length * Math.random())]);
-    console.log($randomSquare);
+    $randomSquare = $($li[Math.floor($li.length * Math.random())]).addClass('selected');
+    // console.log($randomSquare);
   }
   pickRandom();
   //
   // Change class name
-  function changeClass () {
-    $randomSquare.addClass('selected');
-  }
-  changeClass();
+  // function changeClass () {
+  //   $randomSquare.addClass('selected');
+  // }
+  // changeClass();
 
   // Remove new class on timeout
   function removeClass () {
     $randomSquare.removeClass('selected');
   }
-
   function timeOut () {
-    timer = setTimeout(removeClass, 1000);
+    timer = setTimeout(removeClass, 2000);
   }
   timeOut();
 
   // Add click event
-  function clickedOn() {
-    $randomSquare.on('click', removeClass);
+  function clickedOn () {
+    $randomSquare.on('click', removeClass, updateScore);
   }
+
   clickedOn();
 
-  //   setTimeout(() => {
-  //   console.log('setTimeout fired!');
-  //   console.log(new Date());
-  // }, 1000);
+  // Update score
+  function updateScore () {
+    score++;
+    $score.html(score);
+  }
+
 
 
 
